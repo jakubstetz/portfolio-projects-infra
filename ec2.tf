be89a8b26fab9b5c31ba57d078d4b4523689e778
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-06-26 21:17:23 -0600

    chore: define EC2 instance

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "portfolio_insights" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  # Configure root block device
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+    encrypted   = true
+    
+    tags = {
+      Name = "portfolio-insights-root-volume"
+    }
+  }
+
+  user_data = file("scripts/user_data.sh")
+
+  # Recreate the EC2 instance if the user_data script changes,
+  # since user_data only runs on first boot and can't be updated in-place
+  user_data_replace_on_change = true
+}
