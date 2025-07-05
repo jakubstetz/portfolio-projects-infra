200307e66a85e777ef09594821ae4a6a49968986
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-07-05 13:56:31 +0200

    chore: generalize user data script to cover full portfolio

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script runs automatically when EC2 instance boots if provided as User Data
-# It sets up the EC2 instance with everything needed to deploy the backend and microservice with domain and HTTPS routing
+# It sets up the EC2 instance with everything needed to deploy portfolio project backends and microservices with domain and HTTPS routing
 
 # Redirect output to log file
 exec > /var/log/user-data.log 2>&1
@@ -16,12 +16,12 @@ done
 dnf update -y
 dnf install -y git nginx docker python3-pip
 
-# Set up Docker (for backend)
+# Set up Docker
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 
-# Install Go (for microservice)
+# Install Go
 GO_VERSION=1.24.4
 cd /usr/local
 curl -LO https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz
@@ -35,16 +35,20 @@ export PATH=$PATH:/usr/local/go/bin
 cd /home/ec2-user
 git clone https://github.com/jakubstetz/portfolio-insights-backend.git
 git clone https://github.com/jakubstetz/market-service.git
+git clone https://github.com/jakubstetz/resume-scanner.git
 
 # NGINX reverse proxy setup
 cd portfolio-insights-backend/.infra
 cp nginx_portfolio-insights.conf /etc/nginx/conf.d/portfolio-insights.conf
+cd ../
+cd resume-scanner/.infra
+cp nginx_resume-scanner.conf /etc/nginx/conf.d/resume-scanner.conf
 systemctl enable nginx
 systemctl restart nginx
 
 # Certbot setup information message
 echo ""
-echo "Because first-time use of Certbot requires user interaction, SSL setup with Certbot is not included as a part of this script. See SETUP.md for how to set up SSL using Certbot."
+echo "Because first-time use of Certbot requires user interaction, SSL setup with Certbot is not included as a part of this script."
 
 # Set ownership of cloned repositories, to ensure they're not owned by root
 chown -R ec2-user:ec2-user /home/ec2-user
@@ -52,7 +56,6 @@ chown -R ec2-user:ec2-user /home/ec2-user
 # Completion message
 echo ""
 echo "✅ EC2 setup complete."
-echo "Run backend.yaml workflow in GitHub Actions to deploy backend, and then visit https://api.portfolio-insights.jakubstetz.dev/health to check system health."
+echo "Run backend.yaml workflow in GitHub Actions to deploy backend, and then visit https://api.portfolio-insights.jakubstetz.dev/health and https://api.resume-scanner.jakubstetz.dev/health to check system health."
 echo "Run microservice.yaml workflow in GitHub Actions to deploy microservice."
-echo "Note that database will need to be set up separately."
 echo ""
