fd33ce79f30b0d75613c61cd1650e7e9fff75c26
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-07-12 10:43:45 +0200

    chore: remove redundant hardcoded variables from root block device config

--- ec2.tf
+++ ec2.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "portfolio_insights" {
 
   # Configure root block device
   root_block_device {
-    volume_size = 8
-    volume_type = "gp3"
     encrypted   = true
     
     tags = {
