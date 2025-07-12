bd2747b32c2050657eb629817241381ebbb62a41
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-07-12 14:20:31 +0200

    minor: rename EC2 resource

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "portfolio_insights" {
+resource "aws_instance" "portfolio_projects" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
