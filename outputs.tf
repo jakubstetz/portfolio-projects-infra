95ca5791628e68d3b5a3718924a217c63e70be79
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-07-12 14:36:25 +0200

    fix: update outdated resource names

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "ec2_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.portfolio_insights.public_ip
+  value       = aws_instance.portfolio_projects.public_ip
 }
 
 output "ec2_public_dns" {
   description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.portfolio_insights.public_dns
+  value       = aws_instance.portfolio_projects.public_dns
 }
 
 output "ec2_private_ip" {
   description = "The private IP address of the EC2 instance"
-  value       = aws_instance.portfolio_insights.private_ip
+  value       = aws_instance.portfolio_projects.private_ip
 }
 
 output "rds_endpoint" {
   description = "The RDS instance endpoint"
-  value       = aws_db_instance.portfolio_db.endpoint
+  value       = aws_db_instance.portfolio_projects_db.endpoint
 }
